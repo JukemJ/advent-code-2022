201be1ed9e11f2a2ebbc4475a67f5d4082cf19c7
JukemJ <93483466+JukemJ@users.noreply.github.com>
2022-12-04 19:01:57 -0500

    day 4 complete!

---
+++ 04code2.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync('04input.txt').toString().split('\n').map(x => x.split(',').map(y => y.split('-')))
+let count = 0
+for(let pair of input){
+    if ((+pair[1][0] <= +pair[0][1] && +pair[1][0] >= +pair[0][0]) || 
+        (+pair[0][0] <= +pair[1][1] && +pair[0][0] >= +pair[1][0])){
+            //console.log(pair)
+            count++
+        }
+}
+console.log(count)
